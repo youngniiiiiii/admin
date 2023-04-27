a987ab12ec56177337586cc79cfafaeb639cf46c
youngeunkwon <youngnikk4929@gmail.com>
2023-04-27 09:09:30 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,32 @@
+package com.kbstar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String main() {
+        return "index";
+    }
+
+    @RequestMapping("/charts")
+    public String charts(Model model) {
+        model.addAttribute("contents", "charts");
+        return "index";
+    }
+
+    @RequestMapping("/tables")
+    public String tables(Model model) {
+        model.addAttribute("contents", "tables");
+        return "index";
+    }
+
+    @RequestMapping("/livechart")
+    public String livechart(Model model) {
+        model.addAttribute("contents", "livechart");
+        return "index";
+    }
+}
+
